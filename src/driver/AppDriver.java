1550b9260b829f18d75a8137a0db71488494bc68
DavidConsidine <dmconsie@gmail.com>
2016-10-12 20:50:45 +0100

    Refactor bad code smells in src/driver/AppDriver.java

--- src/driver/AppDriver.java
+++ src/driver/AppDriver.java
@@ -5,6 +5,14 @@ import userinterface.StartUpUI;
 
 public class AppDriver {
 
+    /**
+     * Private constructor to hide implicit public constructor
+     * */
+    private AppDriver(){}
+
+    /**
+     *  Launches program
+     * */
     public static void main(String[] args)
     {
         /* Main: Boots User Interface */
