297160cdc5f45a57baf2465b9b55daf196f77525
DavidConsidine <dmconsie@gmail.com>
2016-10-22 20:02:03 +0100

    Update ClientRequestDispatcher class.
    Add static ClientRequestDispatcher member variable.
    Add private constructor.
    Add interceptors container.
    Utilise singleton pattern to return singular instance of class using getInstance() method.

--- src/core/utils/ClientRequestDispatcher.java
+++ src/core/utils/ClientRequestDispatcher.java
@@ -1,12 +1,28 @@
 package core.utils;
 
+import java.util.Vector;
+
 /**
  * Created by David on 22/10/2016.
  */
 public class ClientRequestDispatcher implements ClientRequestInterceptor {
+    private static ClientRequestDispatcher dispatcher;
+    Vector interceptors;
+
+    private ClientRequestDispatcher(){
+        interceptors = new Vector();
+    }
+
+    public static ClientRequestDispatcher getInstance(){
+        if(dispatcher == null){
+            dispatcher = new ClientRequestDispatcher();
+        }
+        return dispatcher;
+    }
+
     @Override
     public void onPreMarshalRequest(UnmarshaledRequest contextObject) {
-        
+
     }
 
     @Override
