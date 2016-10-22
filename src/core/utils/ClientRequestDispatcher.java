1d7d7248c80cfb05ca30ebc4c23c23f6b00fb55b
DavidConsidine <dmconsie@gmail.com>
2016-10-22 19:54:59 +0100

    Update ClientRequestDispacher to implement ClientRequestInterceptor.

--- src/core/utils/ClientRequestDispatcher.java
+++ src/core/utils/ClientRequestDispatcher.java
@@ -3,5 +3,14 @@ package core.utils;
 /**
  * Created by David on 22/10/2016.
  */
-public class ClientRequestDispatcher {
+public class ClientRequestDispatcher implements ClientRequestInterceptor {
+    @Override
+    public void onPreMarshalRequest(UnmarshaledRequest contextObject) {
+        
+    }
+
+    @Override
+    public void onPostMarshalRequest(MarshaledRequest contextObject) {
+
+    }
 }
