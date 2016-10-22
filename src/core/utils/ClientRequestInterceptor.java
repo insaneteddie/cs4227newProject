2f30a67d69ed6b4ad19e5ff88e8dc625cd20be69
DavidConsidine <dmconsie@gmail.com>
2016-10-22 19:48:29 +0100

    Update ClientRequestInterceptor interface with method definitions

--- src/core/utils/ClientRequestInterceptor.java
+++ src/core/utils/ClientRequestInterceptor.java
@@ -4,4 +4,6 @@ package core.utils;
  * Created by David on 22/10/2016.
  */
 public interface ClientRequestInterceptor {
+    void onPreMarshalRequest(UnmarshaledRequest contextObject);
+    void onPostMarshalRequest(MarshaledRequest contextObject);
 }
