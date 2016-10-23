91cba9736a40c16ef9c5e769a5ac3578a6d03398
DavidConsidine <dmconsie@gmail.com>
2016-10-23 11:49:54 +0100

    Add implementation to PlayerInvitesRetrieveCommand.

--- src/core/utils/PlayerInvitesRetrieveCommand.java
+++ src/core/utils/PlayerInvitesRetrieveCommand.java
@@ -1,16 +1,23 @@
 package core.utils;
 
+import core.session.SessionInformation;
+
 /**
  * Created by David on 23/10/2016.
  */
 public class PlayerInvitesRetrieveCommand implements ICommand {
+    private final String name;
+
+    public PlayerInvitesRetrieveCommand(String name){
+        this.name = name;
+    }
     @Override
     public void execute() {
-
+        SessionInformation.getInstance().getPlayerInvites();
     }
 
     @Override
     public String getCommandName() {
-        return null;
+        return name;
     }
 }
