f51567a9d82e425fae778054ea027e510f7d4902
DavidConsidine <dmconsie@gmail.com>
2016-10-23 11:40:55 +0100

    Add implementation to PartyAddCommand.

--- src/core/utils/PartyAddCommand.java
+++ src/core/utils/PartyAddCommand.java
@@ -1,16 +1,33 @@
 package core.utils;
 
+import core.session.SessionInformation;
+
 /**
  * Created by David on 23/10/2016.
  */
 public class PartyAddCommand implements ICommand {
+    private final String name;
+    private int userId;
+
+    public PartyAddCommand(String name){
+        this.name = name;
+    }
+
     @Override
     public void execute() {
+        SessionInformation.getInstance()
+                .addPlayerToParty(SessionInformation.getInstance()
+                        .getPartyIDFromSenderInvite(userId));
+        userId = 0;
+    }
 
+    public void execute(int userId){
+        this.userId = userId;
+        execute();
     }
 
     @Override
     public String getCommandName() {
-        return null;
+        return name;
     }
 }
