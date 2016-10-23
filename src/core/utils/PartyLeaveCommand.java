af5207a664658861e52cdb63056948b418bee37b
DavidConsidine <dmconsie@gmail.com>
2016-10-23 11:52:54 +0100

    Add implementation to PartyLeaveCommand.

--- src/core/utils/PartyLeaveCommand.java
+++ src/core/utils/PartyLeaveCommand.java
@@ -1,16 +1,23 @@
 package core.utils;
 
+import core.session.SessionInformation;
+
 /**
  * Created by David on 23/10/2016.
  */
 public class PartyLeaveCommand implements ICommand {
+    private final String name;
+
+    public PartyLeaveCommand(String name){
+        this.name  = name;
+    }
     @Override
     public void execute() {
-
+        SessionInformation.getInstance().leaveParty();
     }
 
     @Override
     public String getCommandName() {
-        return null;
+        return name;
     }
 }
