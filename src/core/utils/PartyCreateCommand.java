c8dc651fbb168b16dc8fa0d2338d619f673c3f34
DavidConsidine <dmconsie@gmail.com>
2016-10-23 11:20:54 +0100

    Create concrete command classes implementing the ICommand interface.
    Create InviteMessageRetrieveCommand.java
    Create InviteRemoveCommand.java
    Create InviteSendCommand.java
    Create PartyAddCommand.java
    Create PartyCreateCommand.java
    Create PartyDetailsRetrieveCommand.java
    Create PartyInvitesRetieveCommand.java
    Create PartyLeaveCommand.java
    Create PartyRemoveCommand.java
    Create PlayerCreateCommand.java
    Create PlayerLogOutCommand.java

---
+++ src/core/utils/PartyCreateCommand.java
@@ -0,0 +1,16 @@
+package core.utils;
+
+/**
+ * Created by David on 23/10/2016.
+ */
+public class PartyCreateCommand implements ICommand {
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public String getCommandName() {
+        return null;
+    }
+}
