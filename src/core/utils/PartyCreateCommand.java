1e65bd1c5ac6254b7a39b6afca2bc662437cc6a1
DavidConsidine <dmconsie@gmail.com>
2016-10-23 11:31:50 +0100

    Add impementation to PartyCreateCommand.

--- src/core/utils/PartyCreateCommand.java
+++ src/core/utils/PartyCreateCommand.java
@@ -1,16 +1,24 @@
 package core.utils;
 
+import core.session.SessionInformation;
+
 /**
  * Created by David on 23/10/2016.
  */
 public class PartyCreateCommand implements ICommand {
+    private String name;
+
+    public PartyCreateCommand(String name){
+        this.name = name;
+    }
+
     @Override
     public void execute() {
-
+        SessionInformation.getInstance().createParty();
     }
 
     @Override
     public String getCommandName() {
-        return null;
+        return name;
     }
 }
