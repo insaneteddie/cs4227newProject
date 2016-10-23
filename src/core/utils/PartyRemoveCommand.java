7b8a989a4e62264ede345f3292596e3cac8d80bd
DavidConsidine <dmconsie@gmail.com>
2016-10-23 12:01:46 +0100

    Add implementation to PartyRemoveCommand.java

--- src/core/utils/PartyRemoveCommand.java
+++ src/core/utils/PartyRemoveCommand.java
@@ -1,16 +1,30 @@
 package core.utils;
 
+import core.session.SessionInformation;
+
 /**
  * Created by David on 23/10/2016.
  */
 public class PartyRemoveCommand implements ICommand{
+    private final String name;
+    private int member_id;
+
+    public PartyRemoveCommand(String name){
+        this.name = name;
+    }
     @Override
     public void execute() {
+        SessionInformation.getInstance().removePlayerFromParty(member_id);
+        member_id = 0;
+    }
 
+    public void execute(int member_id){
+        this.member_id = member_id;
+        execute();
     }
 
     @Override
     public String getCommandName() {
-        return null;
+        return name;
     }
 }
