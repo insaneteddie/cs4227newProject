f9c3006b70687a1a9eb2170027a5b7bd12479448
bloodypinata <shorthair2@gmail.com>
2016-10-24 22:11:04 +0100

    Refactor Friend Message

--- src/message/FriendMessage.java
+++ src/message/FriendMessage.java
@@ -2,40 +2,40 @@ package message;
 
 public class FriendMessage extends Message
 {
-    public FriendMessage(int senderID, int receiverID, int partyID)
+    private int senderID;
+    private int receiverID;
+
+    public FriendMessage(int senderID, int receiverID, String message)
     {
         this.senderID = senderID;
         this.receiverID = receiverID;
-        this.partyID = partyID;
+        this.message = message;
     }
-    @Override
+
     public int getSenderID() {
         return senderID;
     }
 
-    @Override
     public int getReceiverID() {
         return receiverID;
     }
 
-    @Override
     public String getMessage() {
         return "Message received from User " + senderID;
     }
 
-    @Override
-    public boolean equals(Object otherInvite) {
-        if(otherInvite != null) {
-            PartyInvite p = (PartyInvite) otherInvite;
-            return senderID == p.getSenderID() && partyID == p.partyID;
+    public int getID()
+    {
+        return this.mId;
+    }
+
+    public boolean equals(Object message) {
+        if(message != null) {
+            Message m = (Message) message;
+            return this.mId == m.getID();
         }
         else
             return false;
-    }
-
-    @Override
-    public int getPartyID() {
-        return partyID;
     }
 
     /**
