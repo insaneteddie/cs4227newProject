ddb8ea54aef46e10d68731a3eeda3f35ae5f4e7f
bloodypinata <shorthair2@gmail.com>
2016-10-24 23:56:35 +0100

    Implement friend message

--- src/message/FriendMessage.java
+++ src/message/FriendMessage.java
@@ -2,34 +2,36 @@ package message;
 
 public class FriendMessage extends Message
 {
-    private int senderID;
-    private int receiverID;
 
     public FriendMessage(int senderID, int receiverID, String message)
     {
         this.senderID = senderID;
-        this.receiverID = receiverID;
+        this.receiverID.add(receiverID);
         this.message = message;
     }
 
-    public int getSenderID() {
+    public int getSenderID()
+    {
         return senderID;
     }
 
-    public int getReceiverID() {
-        return receiverID;
+    public int getReceiverID()
+    {
+        return receiverID.get(0);
     }
 
-    public String getMessage() {
-        return "Message received from User " + senderID;
+    public String getMessage()
+    {
+        return message;
     }
 
     public int getID()
     {
-        return this.mId;
+        return mId;
     }
 
-    public boolean equals(Object message) {
+    public boolean equals(Object message)
+    {
         if(message != null) {
             Message m = (Message) message;
             return this.mId == m.getID();
@@ -42,7 +44,8 @@ public class FriendMessage extends Message
      * @return
      * */
     @Override
-    public int hashCode(){
+    public int hashCode()
+    {
         return super.hashCode();
     }
 }
