62784328b7530d521f152da2daf5235bef62663b
DavidConsidine <dmconsie@gmail.com>
2016-10-26 20:25:38 +0100

    Document public method.

--- src/core/utils/ClientRequestInterceptor.java
+++ src/core/utils/ClientRequestInterceptor.java
@@ -4,6 +4,9 @@ package core.utils;
  * Created by David on 22/10/2016.
  */
 public interface ClientRequestInterceptor {
+    /**
+     * @param contextObject 
+     * */
     void onPreMarshalRequest(UnmarshaledRequest contextObject);
     void onPostMarshalRequest(MarshaledRequest contextObject);
 }
