88ea7d3cb607f56e7f0d8e5d0da794d7bb90c338
DavidConsidine <dmconsie@gmail.com>
2016-10-26 19:55:43 +0100

    Document public method.

--- src/core/session/SessionObserver.java
+++ src/core/session/SessionObserver.java
@@ -5,5 +5,6 @@ import core.user.SessionSubject;
 /** Interface for implementing the observer side of the Observer design pattern*/
 public interface SessionObserver
 {
+    /** Abstract method for updating observers of the passed in subject */
     void update(SessionSubject s);
 }
