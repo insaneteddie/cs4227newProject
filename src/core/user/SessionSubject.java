aeb00e64d2f70bb99c4a22ec663cb60408e273ea
DavidConsidine <dmconsie@gmail.com>
2016-10-26 20:13:32 +0100

    Document public interface.

--- src/core/user/SessionSubject.java
+++ src/core/user/SessionSubject.java
@@ -4,6 +4,7 @@ import core.session.SessionObserver;
 
 import java.util.List;
 
+/** interface for subject part of the Observer Design Pattern*/
 public interface SessionSubject
 {
     void attach(SessionObserver o);
