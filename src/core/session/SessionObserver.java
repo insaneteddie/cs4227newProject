b0c48f3a6fb075960f4e41e51a1fee80a7d68110
DavidConsidine <dmconsie@gmail.com>
2016-10-26 19:54:44 +0100

    Document public interface.

--- src/core/session/SessionObserver.java
+++ src/core/session/SessionObserver.java
@@ -2,6 +2,7 @@ package core.session;
 
 import core.user.SessionSubject;
 
+/** Interface for implementing the observer side of the Observer design pattern*/
 public interface SessionObserver
 {
     void update(SessionSubject s);
