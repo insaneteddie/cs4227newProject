4346ea943ae2f660712d28aed9c513d35420b758
DavidConsidine <dmconsie@gmail.com>
2016-10-26 19:57:32 +0100

    Add @FunctionalInterface to class.

--- src/core/session/SessionObserver.java
+++ src/core/session/SessionObserver.java
@@ -3,6 +3,7 @@ package core.session;
 import core.user.SessionSubject;
 
 /** Interface for implementing the observer side of the Observer design pattern*/
+@FunctionalInterface
 public interface SessionObserver
 {
     /** Abstract method for updating observers of the passed in subject */
