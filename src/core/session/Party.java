52684fef762131c1aa713565164f33bc3e8f16df
DavidConsidine <dmconsie@gmail.com>
2016-10-26 19:43:45 +0100

    Document public method.

--- src/core/session/Party.java
+++ src/core/session/Party.java
@@ -33,6 +33,10 @@ public class Party implements SessionObserver {
         }
     }
 
+    /**
+     * @param id
+     * @return 
+     * */
     public boolean isPartyLeader(int id) {
         return (leaderID == id);
     }
