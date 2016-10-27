be4478a32e4667d74bb55d066ce0629eeaf1e5b4
DavidConsidine <dmconsie@gmail.com>
2016-10-27 12:49:04 +0100

    Document public methods.

--- src/core/utils/ICommand.java
+++ src/core/utils/ICommand.java
@@ -4,6 +4,12 @@ package core.utils;
  * Created by David on 22/10/2016.
  */
 public interface ICommand {
+
+    /** abstract method to be overriden by concrete command class to execute specific commands*/
     void execute();
+
+    /**
+     * @return
+     * */
     String getCommandName();
 }
