799518c116caa50030464633d6e2c792f262baa8
CianBolster <cpbolster@gmail.com>
2016-11-03 14:07:07 +0000

    Renaming
    Renaming of ConcreteInfoRequest to SimpleLog to better reflect its intent

---
+++ src/core/interceptor/SimpleLog.java
@@ -0,0 +1,31 @@
+package core.interceptor;
+
+/**
+ * Created by Cian Bolster on 01/11/2016.
+ */
+public class SimpleLog extends AbstractLoggingRequest {
+
+    /** public constructor
+     * @param type int
+     * */
+    public SimpleLog(int type) {
+        this.type = type;
+    }
+
+    /**
+     * @param severity Severity
+     * @param exception Exception
+     * @param message String
+     * @return String
+     * */
+    @Override
+    public String messageCreation(Severity severity, Exception exception, String message) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(severity);
+        sb.append(exception);
+        sb.append(message);
+
+        return sb.toString();
+    }
+}
