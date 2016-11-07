76deaf46cd8189c4f718d0b86cf2eb654575e40f
bloodypinata <shorthair2@gmail.com>
2016-11-07 18:07:55 +0000

    Refactor MessageFactory
    wrote param comments and made changes to the messageType strings

--- src/messaging/MessageFactory.java
+++ src/messaging/MessageFactory.java
@@ -5,15 +5,29 @@ package messaging;
  */
 class MessageFactory extends AbstractMessagingFactory {
 
+    /**
+     * @param messageType String messageType
+     * @param senderId int senderID
+     * @param receiverID int receiverID
+     * @param partyID int partyID
+     * @return new Invite
+     */
     @Override
     public Invite createInvite(String messageType, int senderId, int receiverID, int partyID) {return null;}
 
+    /**
+     * @param messageType String messageType
+     * @param senderId int senderID
+     * @param receiverID int receiverID
+     * @param message String message
+     * @return new Message
+     */
     @Override
     public Message createMessage(String messageType, int senderId, int receiverID, String message) {
         switch (messageType) {
-            case "FriendMessage":
+            case "FRIEND_MESSAGE":
                 return new FriendMessage(senderId, receiverID, message);
-            case "PartyMessage":
+            case "PARTY_MESSAGE":
                 return new PartyMessage(senderId, receiverID, message);
             default:
                 // will log
