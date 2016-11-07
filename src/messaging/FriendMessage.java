36189138d5dde60ff8a785a8724dbc08672f616a
bloodypinata <shorthair2@gmail.com>
2016-11-07 18:17:17 +0000

    Refactor FriendMessage class
    put in param comments, hooks for future expansion to allow for removal of an object by its ID, and methods to get the message

--- src/messaging/FriendMessage.java
+++ src/messaging/FriendMessage.java
@@ -1,5 +1,7 @@
 package messaging;
 
+import java.util.UUID;
+
 /**
  * Created by Christian on 03/11/2016.
  */
@@ -7,32 +9,67 @@ class FriendMessage implements Message {
     private int senderID;
     private int receiverID;
     private String message;
+    private UUID id;
 
-    public FriendMessage(int senderId, int receiverID, String message) {
+    /**
+     * @param senderId int senderID
+     * @param receiverID receiverID
+     * @param message String message
+     */
+    FriendMessage(int senderId, int receiverID, String message) {
         this.senderID = senderId;
         this.receiverID = receiverID;
         this.message = message;
     }
 
+    /**
+     * communicates the message to the DB
+     */
     @Override
     public void sendMessage() {
         // communicates the message to the DB
     }
 
+    /**
+     * @return id
+     */
     @Override
-    public int getID() {
+    public UUID getID() {
         //call to db to get ID
-        return 0;
+        return id;
     }
 
+    /**
+     * @return senderID
+     */
     @Override
     public int getSenderID() {
         return senderID;
     }
 
+    /**
+     * @return partyID which is 0 for all messages
+     */
+    @Override
+    public int getPartyID() {
+        return 0;
+    }
+
+    /**
+     * @return receiverID
+     */
     @Override
     public int getReceiverID()
     {
         return receiverID;
+    }
+
+    /**
+     * @return the message within the Message object
+     */
+    @Override
+    public String getMessage()
+    {
+        return message;
     }
 }
