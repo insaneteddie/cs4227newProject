c1094c4d29b8bca458484f88f5594450ff7d2763
bloodypinata <shorthair2@gmail.com>
2016-11-07 18:06:13 +0000

    Refactor MessageCollection interface
    put in hooks for future expansion to allow for removal of an object by its id

--- src/messaging/InviteFactory.java
+++ src/messaging/InviteFactory.java
@@ -5,19 +5,33 @@ package messaging;
  */
 class InviteFactory extends AbstractMessagingFactory {
 
+    /**
+     * @param inviteType String inviteType
+     * @param senderId int senderID
+     * @param receiverID int receiverID
+     * @param partyID int partyID
+     * @return new Invite
+     */
     @Override
     public Invite createInvite(String inviteType, int senderId, int receiverID, int partyID) {
         switch (inviteType){
-            case "PartyInvite":
+            case "PARTY_INVITE":
                 return new PartyInvite(senderId, receiverID, partyID);
-            case "FriendInvite" :
+            case "FRIEND_INVITE" :
                 return new FriendInvite(senderId,receiverID, partyID);
             default:
                 // will log
-                throw new IllegalArgumentException("Invalid Invite type given:" + inviteType);
+                throw new IllegalArgumentException("Invalid Invite type given: " + inviteType);
         }
     }
 
+    /**
+     * @param messageType String messageType
+     * @param senderId int senderID
+     * @param receiverID int receiverID
+     * @param message String Message
+     * @return new Message
+     */
     @Override
     public Message createMessage(String messageType, int senderId, int receiverID, String message) {return null;}
 }
