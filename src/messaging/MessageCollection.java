739296330b11782992c5ca82e48349e4be2f5948
bloodypinata <shorthair2@gmail.com>
2016-11-07 18:04:28 +0000

    Refactor MessageCollection interface
    put in hooks for future expansion to allow for removal of an object by its id

--- src/messaging/MessageCollection.java
+++ src/messaging/MessageCollection.java
@@ -2,6 +2,7 @@ package messaging;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Christian on 03/11/2016.
@@ -10,21 +11,31 @@ public class MessageCollection implements Collection<Message>
 {
     private ArrayList<Message> messages;
 
+    /**
+     *
+     */
     public MessageCollection() {
         messages = new ArrayList<>();
     }
 
+    /**
+     * @param item Message item
+     */
     @Override
     public void add(Message item) {
         messages.add(item);
     }
 
+    /**
+     * @param id UUID id
+     * @return Message
+     */
     @Override
-    public Message get(int id)
+    public Message get(UUID id)
     {
         for(Message m : messages)
         {
-            if(id == m.getID())
+            if(id.equals(m.getID()))
             {
                 return m;
             }
@@ -32,17 +43,38 @@ public class MessageCollection implements Collection<Message>
         return null;
     }
 
+    /**
+     * @return List of Messages
+     */
     @Override
     public List<Message> getAll() {
         return messages;
     }
 
+    /**
+     * @param id UUID id
+     */
     @Override
-    public void remove(int id)
+    public void remove(UUID id)
     {
         for(Message m : messages)
         {
-            if(id == m.getID())
+            if(id.equals(m.getID()))
+            {
+                messages.remove(m);
+            }
+        }
+    }
+
+    /**
+     * @param senderID int senderID
+     * @param partyID int partyID
+     */
+    @Override
+    public void remove(int senderID, int partyID) {
+        for (Message m : messages)
+        {
+            if(senderID == m.getSenderID() && partyID == m.getPartyID())
             {
                 messages.remove(m);
             }
