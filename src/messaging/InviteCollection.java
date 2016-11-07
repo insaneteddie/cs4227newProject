1cdaafc27d23a9c401ef888f9eaad3a7e9d9829e
bloodypinata <shorthair2@gmail.com>
2016-11-07 17:58:49 +0000

    Refactor InviteCollection interface
    put in hooks for future expansion to allow for removal of an object by its id

--- src/messaging/InviteCollection.java
+++ src/messaging/InviteCollection.java
@@ -2,6 +2,7 @@ package messaging;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Christian on 03/11/2016.
@@ -10,21 +11,30 @@ public class InviteCollection implements Collection<Invite>
 {
     private ArrayList<Invite> invites;
 
+    /**
+     */
     public InviteCollection() {
         invites = new ArrayList<>();
     }
 
+    /**
+     * @param item Invite invite
+     */
     @Override
     public void add(Invite item) {
         invites.add(item);
     }
 
+    /**
+     * @param id UUID id
+     * @return invite
+     */
     @Override
-    public Invite get(int id)
+    public Invite get(UUID id)
     {
         for (Invite i : invites)
         {
-            if(id == i.getID())
+            if(id.equals(i.getID()))
             {
                 return i;
             }
@@ -32,19 +42,44 @@ public class InviteCollection implements Collection<Invite>
         return null;
     }
 
+    /**
+     * @return list of invites
+     */
     @Override
     public List<Invite> getAll() {
-        return null;
+        return invites;
     }
 
+    /**
+     * @param id UUID id
+     */
     @Override
-    public void remove(int id)
+    public void remove(UUID id)
     {
         for (Invite i : invites)
         {
-            if(id == i.getID())
+            if(id.equals(i.getID()))
             {
                 invites.remove(i);
+                i.deleteInvite();
+            }
+        }
+    }
+
+    /**
+     * @param senderID int senderID
+     * @param partyID int partyID
+     */
+    @Override
+    public void remove(int senderID, int partyID)
+    {
+        for (Invite i : invites)
+        {
+            if(senderID == i.getSenderID() && partyID == i.getPartyID())
+            {
+                invites.remove(i);
+                //delete message from DB
+                i.deleteInvite();
             }
         }
     }
