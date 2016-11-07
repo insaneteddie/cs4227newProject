0c0422ed98ec8d605e36c07441e788b11956a683
bloodypinata <shorthair2@gmail.com>
2016-11-07 18:24:28 +0000

    Refactor FriendInvite class
    put in param comments, hooks for future expansion to allow for removal of an object by its ID, the ability to delete objects, and now instantiate the DB object instead of sending it in from the sessionInformation, was null if trying to remove an invite before having send one

--- src/messaging/FriendInvite.java
+++ src/messaging/FriendInvite.java
@@ -1,6 +1,9 @@
 package messaging;
 
-import database.DatabaseInterface;
+import database.DatabaseBridge;
+import core.utils.Log;
+
+import java.util.UUID;
 
 /**
  * Created by Christian on 03/11/2016.
@@ -8,39 +11,87 @@ import database.DatabaseInterface;
 class FriendInvite implements Invite{
     private int senderID;
     private int receiverID;
+    private UUID messageID;
+    private int partyID = 0;
+    private DatabaseBridge sqlDB = new DatabaseBridge();
+    private final Log log = new Log(getClass().getName());
 
+    /**
+     * @param senderId int senderID
+     * @param receiverID int receiverID
+     * @param partyID int partyID
+     */
     FriendInvite(int senderId, int receiverID, int partyID) {
         this.senderID = senderId;
         this.receiverID = receiverID;
+        messageID = UUID.randomUUID();
     }
 
+    /**
+     * @return messageID
+     */
     @Override
-    public int getID() {
-        return 0;
+    public UUID getID() {
+        return messageID;
     }
 
+    /**
+     * sends the invite to the DB
+     */
     @Override
-    public void sendInvite(DatabaseInterface database) {
-        // need to com with DB
+    public void sendInvite() {
+        try {
+            sqlDB.addInvite(senderID, receiverID, partyID);
+            log.logInfo("Party Invite Sent");
+        } catch (Exception ex) {
+            log.logWarning(ex);
+        }
+
     }
 
+    /**
+     * @return senderID
+     */
     @Override
     public int getSenderID() {
         return senderID;
     }
 
+    /**
+     * @return receiverID
+     */
     @Override
     public int getReceiverID() {
         return receiverID;
     }
 
+    /**
+     * @return partyID
+     */
     @Override
     public int getPartyID() {
         return 0;
     }
 
+    /**
+     * @return the message for the invite
+     */
     @Override
     public String getMessage() {
         return "User " + senderID + " would like to add you to their friends list";
+    }
+
+    /**
+     * deletes an invite from the DB
+     */
+    @Override
+    public void deleteInvite()
+    {
+        try {
+            sqlDB.removeInvite(senderID, receiverID, partyID);
+            log.logInfo("Invite from " + senderID + " to " + receiverID + " has been deleted");
+        } catch (Exception ex) {
+            log.logWarning(ex);
+        }
     }
 }
